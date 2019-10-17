faad6eeb46a910d625f3151cc18bbffb1627616a
Nathan Mousa <nathanmousa1@gmail.com>
2019-10-17 17:05:43 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,8 @@
-# Add  code here!
+def prime?(num)
+  if num <= 1 #To count for negative numbers
+    return false
+  elsif num <= 3
+    return true
+  else (2..num/2).none? { |x| num % x == 0}
+  end
+end
